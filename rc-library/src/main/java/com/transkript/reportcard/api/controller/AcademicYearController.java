5c3709335c92d55ec5530c94fbd07fa58f6a0351
andrew21-mch <nfonandrew73@gmail.com>
2022-05-04 11:08:59 +0100

    fixed imports for api/controllers

--- rc-library/src/main/java/com/transkript/reportcard/api/controller/AcademicYearController.java
+++ rc-library/src/main/java/com/transkript/reportcard/api/controller/AcademicYearController.java
@@ -1,6 +1,10 @@
 package com.transkript.reportcard.api.controller;
 
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @AllArgsConstructor
 @RequestMapping(value = "/api/academicyear")
--- rc-library/src/main/java/com/transkript/reportcard/api/controller/StudentController.java
+++ rc-library/src/main/java/com/transkript/reportcard/api/controller/StudentController.java
@@ -1,5 +1,8 @@
 package com.transkript.reportcard.api.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
