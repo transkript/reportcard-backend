4bf566a799a41465a18c1b8393c818fa866cae8d
Elroy Kanye <elroykanye@gmail.com>
2022-05-06 18:43:07 +0100

    added mapper and tests for Term

--- rc-library/src/main/java/com/transkript/reportcard/api/dto/TermDto.java
+++ rc-library/src/main/java/com/transkript/reportcard/api/dto/TermDto.java
@@ -1,13 +1,23 @@
 package com.transkript.reportcard.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Builder
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class TermDto {
+    @JsonProperty("id")
     private Long id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("academic_year_id")
+    private Long academicYearId;
 }
