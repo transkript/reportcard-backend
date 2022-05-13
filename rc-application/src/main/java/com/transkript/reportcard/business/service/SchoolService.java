a3b7d5be97bb4b4fb091c97e1cdaee00606394e4
andrew21-mch <nfonandrew73@gmail.com>
2022-05-13 23:39:39 +0100

    school api service

--- rc-application/src/main/java/com/transkript/reportcard/business/service/SchoolService.java
+++ rc-application/src/main/java/com/transkript/reportcard/business/service/SchoolService.java
@@ -1,5 +1,19 @@
 package com.transkript.reportcard.business.service;
 
 
+import com.transkript.reportcard.api.dto.SchoolDto;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
 public interface SchoolService {
+    String addSchool(SchoolDto schoolDto);
+
+    List<SchoolDto> getSchools();
+
+    SchoolDto getSchool(Long id);
+
+    String updateSchool(Long id, SchoolDto schoolDto);
+
+    String deleteSchool(Long id);
 }
