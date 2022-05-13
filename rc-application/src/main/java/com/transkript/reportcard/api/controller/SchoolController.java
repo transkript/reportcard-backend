14c912cfd1649754d80661a3bb2b9937cee9ec07
andrew21-mch <nfonandrew73@gmail.com>
2022-05-13 23:39:21 +0100

    school api controller

--- rc-application/src/main/java/com/transkript/reportcard/api/controller/SchoolController.java
+++ rc-application/src/main/java/com/transkript/reportcard/api/controller/SchoolController.java
@@ -1,11 +1,51 @@
 package com.transkript.reportcard.api.controller;
 
+import com.transkript.reportcard.api.dto.SchoolDto;
+import com.transkript.reportcard.business.service.SchoolService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Slf4j
 @RestController
 @AllArgsConstructor
 @RequestMapping(value = "/api/school")
 public class SchoolController {
+
+    private final SchoolService schoolService;
+
+
+    @PostMapping
+    public ResponseEntity<String> addSchool(@RequestBody SchoolDto schoolDto){
+        log.info("Adding school with name "+ schoolDto.getName());
+        return new ResponseEntity<String>(schoolService.addSchool(schoolDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SchoolDto>> getSchools(){
+        log.info("Getting all schools");
+        return ResponseEntity.ok(schoolService.getSchools());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SchoolDto> getSchool(@PathVariable("id") Long id){
+        log.info("Getting school with Id: " + id);
+        return ResponseEntity.ok(schoolService.getSchool(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateSchool( @PathVariable("id") Long id, @RequestBody SchoolDto schoolDto){
+        log.info("Updating school with ID: "+ id);
+        return ResponseEntity.ok(schoolService.updateSchool(id, schoolDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteSchool(@PathVariable("id") Long id){
+        log.info("Deleting School with ID: "+id);
+        return ResponseEntity.ok(schoolService.deleteSchool(id));
+    }
 }
