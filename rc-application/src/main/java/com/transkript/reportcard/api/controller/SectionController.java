5e2b7f4985ebb95bb6700a535930dc02ddb12875
Tanju Brunostar <tanjubrunostar0@gmail.com>
2022-05-16 21:46:27 +0100

    wrote a REST API for section

--- rc-application/src/main/java/com/transkript/reportcard/api/controller/SectionController.java
+++ rc-application/src/main/java/com/transkript/reportcard/api/controller/SectionController.java
@@ -1,11 +1,52 @@
 package com.transkript.reportcard.api.controller;
 
+import com.transkript.reportcard.api.dto.SectionDto;
+import com.transkript.reportcard.business.service.SectionService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+
+@Slf4j
 @RestController
 @AllArgsConstructor
 @RequestMapping(value = "/api/section")
 public class SectionController {
+
+    private final SectionService sectionService;
+
+    @PostMapping
+    public ResponseEntity<String> addSection(@RequestBody SectionDto sectionDto){
+        log.info("Adding section with name "+ sectionDto.getName());
+        return new ResponseEntity<String>(sectionService.addSection(sectionDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SectionDto>> getSections() {
+        log.info("Getting the sections");
+        return ResponseEntity.ok(sectionService.getSections());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SectionDto> getSection(@PathVariable("id") Long id) {
+        log.info("Getting the section with ID: " + id);
+        return ResponseEntity.ok(sectionService.getSection(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateSection(@PathVariable("id") Long id,
+                                                @RequestBody SectionDto sectionDto) {
+        log.info("Updating section with name ID: " + id);
+        return ResponseEntity.ok(sectionService.updateSection(id, sectionDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteSection(@PathVariable("id") Long id) {
+        log.info("Deleting section with id: " + id);
+        return ResponseEntity.ok(sectionService.deleteSection(id));
+    }
 }
--- rc-application/src/main/java/com/transkript/reportcard/business/service/impl/SectionServiceImpl.java
+++ rc-application/src/main/java/com/transkript/reportcard/business/service/impl/SectionServiceImpl.java
@@ -1,10 +1,79 @@
 package com.transkript.reportcard.business.service.impl;
+
+import com.transkript.reportcard.api.dto.SectionDto;
+import com.transkript.reportcard.business.mapper.SectionMapper;
+import com.transkript.reportcard.business.service.SchoolService;
 import com.transkript.reportcard.business.service.SectionService;
+import com.transkript.reportcard.data.entity.School;
+import com.transkript.reportcard.data.entity.Section;
+import com.transkript.reportcard.data.repository.SectionRepository;
+import com.transkript.reportcard.exception.EntityException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
 @Getter @Setter
 @AllArgsConstructor
 public class SectionServiceImpl implements SectionService {
+
+    private final SectionRepository sectionRepository;
+    private final SectionMapper sectionMapper;
+    private final SchoolService schoolService;
+
+    @Override
+    public String addSection(SectionDto sectionDto) {
+        School school = schoolService.getSchoolById(sectionDto.getSchoolId());
+
+        Section section = sectionMapper.mapDtoToSection(sectionDto);
+        section.setId(null);
+        section.setSchool(school);
+        sectionRepository.save(section);
+        return "Section created successfully";
+    }
+
+    @Override
+    public List<SectionDto> getSections() {
+        return sectionRepository.findAll().stream()
+                .map(section -> sectionMapper.mapSectionToDto(section))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public SectionDto getSection(Long id) {
+        Optional<Section> sectionOptional = sectionRepository.findById(id);
+        return sectionMapper.mapSectionToDto(sectionOptional.orElseThrow(()->{
+
+            //TODO: Handle this exception
+            throw new RuntimeException();
+        }));
+    }
+
+    @Override
+    public String updateSection(Long id, SectionDto sectionDto) {
+        if (id != null && sectionRepository.existsById(id)) {
+            School school = schoolService.getSchoolById(sectionDto.getSchoolId());
+            Section section = sectionMapper.mapDtoToSection(sectionDto);
+            section.setId(id);
+            section.setSchool(school);
+            sectionRepository.save(section);
+            return "Successfully updated section with ID: " + id;
+        }
+        throw new EntityException.EntityNotFoundException("section");
+    }
+
+    @Override
+    public String deleteSection(Long id) {
+        if (id != null && sectionRepository.existsById(id)) {
+            sectionRepository.deleteById(id);
+            return "Successfully deleted section with ID: " + id;
+        }
+
+        throw new EntityException.EntityNotFoundException("section");
+    }
 }
