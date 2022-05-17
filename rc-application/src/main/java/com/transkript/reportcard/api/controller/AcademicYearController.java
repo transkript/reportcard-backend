46b4464850d3d65722cfd0f7e3880d19d5e17cf6
Tanju Brunostar <tanjubrunostar0@gmail.com>
2022-05-17 12:46:39 +0100

    wrote a REST API for Academic year

--- rc-application/src/main/java/com/transkript/reportcard/api/controller/AcademicYearController.java
+++ rc-application/src/main/java/com/transkript/reportcard/api/controller/AcademicYearController.java
@@ -1,12 +1,56 @@
 package com.transkript.reportcard.api.controller;
 
-
+import com.transkript.reportcard.api.dto.AcademicYearDto;
+import com.transkript.reportcard.business.service.AcademicYearService;
+import com.transkript.reportcard.data.entity.AcademicYear;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Slf4j
 @RestController
 @AllArgsConstructor
 @RequestMapping(value = "/api/academicyear")
 public class AcademicYearController {
+
+    private final AcademicYearService academicYearService;
+
+    @PostMapping
+    public ResponseEntity<String> addAcademicYear(
+            @RequestBody AcademicYearDto academicYearDto) {
+            log.info("Adding Academic Year with name " + academicYearDto.getName());
+        return new ResponseEntity<String>(academicYearService.addAcademicYear(academicYearDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AcademicYearDto>> getAcademicYears() {
+        log.info("Getting the academic years");
+        return ResponseEntity.ok(academicYearService.getAcademicYears());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AcademicYearDto> getAcademicYear(@PathVariable("id") Long id) {
+        log.info("Getting academic year with ID: " + id);
+        return ResponseEntity.ok(academicYearService.getAcademicYear(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateAcademicYear(
+            @RequestBody AcademicYearDto academicYearDto,
+            @PathVariable("id") Long id
+    ) {
+        log.info("Updating academic year with ID: " + id);
+        return ResponseEntity.ok(academicYearService.updateAcademicYear(id, academicYearDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteAcademicYear(
+            @PathVariable("id") Long id) {
+        log.info("Deleting academic year with ID: " + id);
+        return ResponseEntity.ok(academicYearService.deleteAcademicYear(id));
+    }
 }
