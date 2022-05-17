e8bafe8f21a85ca2d5bf452ae3c2fb13aba35849
Tanju Brunostar <tanjubrunostar0@gmail.com>
2022-05-17 12:47:08 +0100

    wrote a REST API for Academic year

--- rc-application/src/main/java/com/transkript/reportcard/business/service/impl/AcademicYearServiceImpl.java
+++ rc-application/src/main/java/com/transkript/reportcard/business/service/impl/AcademicYearServiceImpl.java
@@ -1,12 +1,71 @@
 package com.transkript.reportcard.business.service.impl;
+
+import com.transkript.reportcard.api.dto.AcademicYearDto;
+import com.transkript.reportcard.business.mapper.AcademicYearMapper;
 import com.transkript.reportcard.business.service.AcademicYearService;
+import com.transkript.reportcard.data.entity.AcademicYear;
+import com.transkript.reportcard.data.repository.AcademicYearRepository;
+import com.transkript.reportcard.exception.EntityException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.expression.ExpressionException;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@Service
 public class AcademicYearServiceImpl implements AcademicYearService {
+
+    private final AcademicYearRepository academicYearRepository;
+    private final AcademicYearMapper academicYearMapper;
+
+    @Override
+    public String addAcademicYear(AcademicYearDto academicYearDto) {
+        AcademicYear academicYear = academicYearMapper.mapDtoToAcademicYear(academicYearDto);
+        academicYear.setId(null);
+        academicYearRepository.save(academicYear);
+        return "Added Academic year successfully";
+    }
+
+    @Override
+    public List<AcademicYearDto> getAcademicYears() {
+        return academicYearRepository.findAll().stream()
+                .map(academicYear -> academicYearMapper.mapAcademicYearToDto(academicYear))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public AcademicYearDto getAcademicYear(Long id) {
+        return academicYearMapper.mapAcademicYearToDto(academicYearRepository.findById(id).orElseThrow(
+            ()->{
+                //TODO: Handle this exception
+                throw new RuntimeException();
+            }
+        ));
+    }
+
+    @Override
+    public String updateAcademicYear(Long id, AcademicYearDto academicYearDto) {
+        if (id != null && academicYearRepository.existsById(id)) {
+            AcademicYear academicYear = academicYearMapper.mapDtoToAcademicYear(academicYearDto);
+            academicYear.setId(id);
+            academicYearRepository.save(academicYear);
+            return "Successfully updated Academic year with ID: " + id;
+        }
+        throw new EntityException.EntityNotFoundException("Academic year with id: " + id);
+    }
+
+    @Override
+    public String deleteAcademicYear(Long id) {
+        if (id != null && academicYearRepository.existsById(id)) {
+            academicYearRepository.deleteById(id);
+            return "Successfully deleted academic  year with ID: " + id;
+        }
+        throw new EntityException.EntityNotFoundException("Academic year with id: " + id);
+    }
 }
