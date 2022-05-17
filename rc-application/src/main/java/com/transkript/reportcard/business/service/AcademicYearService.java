ac509419059c3798b1d933c41d764da28bcdc24a
Tanju Brunostar <tanjubrunostar0@gmail.com>
2022-05-17 12:46:53 +0100

    wrote a REST API for Academic year

--- rc-application/src/main/java/com/transkript/reportcard/business/service/AcademicYearService.java
+++ rc-application/src/main/java/com/transkript/reportcard/business/service/AcademicYearService.java
@@ -1,5 +1,20 @@
 package com.transkript.reportcard.business.service;
 
 
+import com.transkript.reportcard.api.dto.AcademicYearDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 public interface AcademicYearService {
+    String addAcademicYear(AcademicYearDto academicYearDto);
+
+    List<AcademicYearDto> getAcademicYears();
+
+    AcademicYearDto getAcademicYear(Long id);
+
+    String updateAcademicYear(Long id, AcademicYearDto academicYearDto);
+
+    String deleteAcademicYear(Long id);
 }
