d354de42b7781d4ba42fecc2f45e084473094067
Tanju Brunostar <tanjubrunostar0@gmail.com>
2022-05-17 14:57:24 +0100

    wrote a REST API for Class level

--- rc-application/src/main/java/com/transkript/reportcard/business/service/ClassLevelService.java
+++ rc-application/src/main/java/com/transkript/reportcard/business/service/ClassLevelService.java
@@ -1,5 +1,19 @@
 package com.transkript.reportcard.business.service;
 
 
+import com.transkript.reportcard.api.dto.ClassLevelDto;
+import com.transkript.reportcard.data.entity.ClassLevel;
+
+import java.util.List;
+
 public interface ClassLevelService {
+    String addClassLevel(ClassLevelDto classLevelDto);
+
+    List<ClassLevelDto> getClassLevels();
+
+    ClassLevelDto getClassLevel(Long id);
+
+    String updateClassLevel(Long id, ClassLevelDto classLevelDto);
+
+    String deleteClassLevel(Long id);
 }
