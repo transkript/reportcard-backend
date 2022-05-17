70ab36cd31c9d361f4d270100d49d2584ce80370
andrew21-mch <nfonandrew73@gmail.com>
2022-05-17 20:08:36 +0100

    implement the add secion api

--- rc-application/src/main/java/com/transkript/reportcard/api/controller/SectionController.java
+++ rc-application/src/main/java/com/transkript/reportcard/api/controller/SectionController.java
@@ -1,11 +1,27 @@
 package com.transkript.reportcard.api.controller;
 
+import com.transkript.reportcard.api.dto.SectionDto;
+import com.transkript.reportcard.business.service.SectionService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @AllArgsConstructor
 @RequestMapping(value = "/api/section")
 public class SectionController {
+    SectionService sectionService;
+
+    @PostMapping
+    public ResponseEntity<String> addSection(@RequestBody SectionDto sectionDto){
+        log.info("Saving Section with name" + sectionDto.getName());
+        return new ResponseEntity<String>(sectionService.addSection(sectionDto), HttpStatus.CREATED) ;
+
+    }
 }
