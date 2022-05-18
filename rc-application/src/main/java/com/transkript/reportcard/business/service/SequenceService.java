9fd7a4a2ae2bb29b0f6f2986fc87148f6bf0907f
Tanju Brunostar <tanjubrunostar0@gmail.com>
2022-05-18 17:43:25 +0100

    wrote a REST API for sequence

--- rc-application/src/main/java/com/transkript/reportcard/business/service/SequenceService.java
+++ rc-application/src/main/java/com/transkript/reportcard/business/service/SequenceService.java
@@ -1,5 +1,18 @@
 package com.transkript.reportcard.business.service;
 
 
+import com.transkript.reportcard.api.dto.SequenceDto;
+
+import java.util.List;
+
 public interface SequenceService {
+    String addSequence(SequenceDto sequenceDto);
+
+    List<SequenceDto> getSequences();
+
+    SequenceDto getSequence(Long id);
+
+    String updateSequence(Long id, SequenceDto sequenceDto);
+
+    String deleteSequence(Long id);
 }
