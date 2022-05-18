0aaf8da458a11563e0ec24f560f54f1ccb7da7f9
Tanju Brunostar <tanjubrunostar0@gmail.com>
2022-05-18 17:43:35 +0100

    wrote a REST API for sequence

--- rc-application/src/main/java/com/transkript/reportcard/business/service/impl/SequenceServiceImpl.java
+++ rc-application/src/main/java/com/transkript/reportcard/business/service/impl/SequenceServiceImpl.java
@@ -1,10 +1,76 @@
 package com.transkript.reportcard.business.service.impl;
+import com.transkript.reportcard.api.dto.SequenceDto;
+import com.transkript.reportcard.business.mapper.SequenceMapper;
 import com.transkript.reportcard.business.service.SequenceService;
+import com.transkript.reportcard.business.service.TermService;
+import com.transkript.reportcard.data.entity.ClassLevelSub;
+import com.transkript.reportcard.data.entity.Sequence;
+import com.transkript.reportcard.data.entity.Term;
+import com.transkript.reportcard.data.repository.SectionRepository;
+import com.transkript.reportcard.data.repository.SequenceRepository;
+import com.transkript.reportcard.exception.EntityException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter @Setter
 @AllArgsConstructor
+@Service
 public class SequenceServiceImpl implements SequenceService {
+
+    private final SequenceRepository sequenceRepository;
+    private final SequenceMapper sequenceMapper;
+    private final TermService termService;
+
+    @Override
+    public String addSequence(SequenceDto sequenceDto) {
+        Sequence sequence = sequenceMapper.mapDtoToSequence(sequenceDto);
+        sequence.setId(null);
+        sequence.setTerm(termService.getTermById(sequenceDto.getTermId()));
+        sequenceRepository.save(sequence);
+        return "Sequence created successfully";
+    }
+
+    @Override
+    public List<SequenceDto> getSequences() {
+        return sequenceRepository.findAll().stream()
+                .map(sequenceMapper::mapSequenceToDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public SequenceDto getSequence(Long id) {
+        return sequenceMapper.mapSequenceToDto(
+                sequenceRepository.findById(id).orElseThrow(
+                        () -> {
+                            throw new EntityException.EntityNotFoundException("Sequence with id " + id);
+                        }
+                )
+        );
+    }
+
+    @Override
+    public String updateSequence(Long id, SequenceDto sequenceDto) {
+        if (id != null && sequenceRepository.existsById(id)) {
+            Sequence sequence = sequenceMapper.mapDtoToSequence(sequenceDto);
+            sequence.setId(id);
+            Term term = termService.getTermById(sequenceDto.getTermId());
+            sequence.setTerm(term);
+            return "Successfully updated sequence with id: " + id;
+        }
+        throw new EntityException.EntityNotFoundException("Sequence with id: " + id);
+    }
+
+    @Override
+    public String deleteSequence(Long id) {
+        if (id != null && sequenceRepository.existsById(id)) {
+            sequenceRepository.deleteById(id);
+            return "Successfully deleted Sequence with id: " + id;
+        }
+        throw new EntityException.EntityNotFoundException("Sequence with id " + id);
+    }
 }
