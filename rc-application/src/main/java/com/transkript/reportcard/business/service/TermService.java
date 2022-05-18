a7cb6a8af78ef4f0d7295c1592ce685ba710c3fe
Tanju Brunostar <tanjubrunostar0@gmail.com>
2022-05-18 17:44:17 +0100

    wrote a REST API for term

--- rc-application/src/main/java/com/transkript/reportcard/business/service/TermService.java
+++ rc-application/src/main/java/com/transkript/reportcard/business/service/TermService.java
@@ -1,5 +1,21 @@
 package com.transkript.reportcard.business.service;
 
 
+import com.transkript.reportcard.api.dto.TermDto;
+import com.transkript.reportcard.data.entity.Term;
+
+import java.util.List;
+
 public interface TermService {
+    String addTerm(TermDto termDto);
+
+    List<TermDto> getTerms();
+
+    TermDto getTerm(Long id);
+
+    Term getTermById(Long id);
+
+    String updateTerm(Long id, TermDto termDto);
+
+    String deleteTerm(Long id);
 }
