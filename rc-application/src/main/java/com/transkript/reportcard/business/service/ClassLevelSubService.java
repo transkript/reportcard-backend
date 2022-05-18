aa5ea586529764d79cfcc662b5d7a6ddc08d5933
Tanju Brunostar <tanjubrunostar0@gmail.com>
2022-05-18 17:41:28 +0100

    wrote a REST API for Class level sub

--- rc-application/src/main/java/com/transkript/reportcard/business/service/ClassLevelSubService.java
+++ rc-application/src/main/java/com/transkript/reportcard/business/service/ClassLevelSubService.java
@@ -1,5 +1,19 @@
 package com.transkript.reportcard.business.service;
 
 
+import com.transkript.reportcard.api.dto.ClassLevelSubDto;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
 public interface ClassLevelSubService {
+    String addClassLevelSub(ClassLevelSubDto classLevelSubDto);
+
+    List<ClassLevelSubDto> getClassLevelSubs();
+
+    ClassLevelSubDto getClassLevelSub(Long id);
+
+    String updateClassLevelSub(Long id, ClassLevelSubDto classLevelSubDto);
+
+    String deleteClassLevelSub(Long id);
 }
