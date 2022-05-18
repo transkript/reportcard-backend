d017b5ce3d66bac847bcf63566c51b5f626867fe
Elroy Kanye <elroykanye@gmail.com>
2022-05-18 13:17:57 +0100

    add api impls for student application

--- rc-application/src/main/java/com/transkript/reportcard/business/service/StudentApplicationService.java
+++ rc-application/src/main/java/com/transkript/reportcard/business/service/StudentApplicationService.java
@@ -1,5 +1,18 @@
 package com.transkript.reportcard.business.service;
 
 
+import com.transkript.reportcard.api.dto.StudentApplicationDto;
+import com.transkript.reportcard.api.dto.response.EntityResponse;
+import com.transkript.reportcard.data.entity.StudentApplication;
+
+import java.util.List;
+
 public interface StudentApplicationService {
+    StudentApplication getStudentApplicationEntity(Long id);
+
+    EntityResponse addStudentApplication(StudentApplicationDto studentApplicationDto);
+
+    List<StudentApplicationDto> getStudentApplications();
+
+    StudentApplicationDto getStudentApplicationById(Long id);
 }
