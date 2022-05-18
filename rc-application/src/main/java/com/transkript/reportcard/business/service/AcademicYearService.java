a15b87f68b536126aa94214ba15c369ea53a177e
Elroy Kanye <elroykanye@gmail.com>
2022-05-18 13:16:39 +0100

    add methods to retrieve academic year entity

--- rc-application/src/main/java/com/transkript/reportcard/business/service/AcademicYearService.java
+++ rc-application/src/main/java/com/transkript/reportcard/business/service/AcademicYearService.java
@@ -1,5 +1,8 @@
 package com.transkript.reportcard.business.service;
 
 
+import com.transkript.reportcard.data.entity.AcademicYear;
+
 public interface AcademicYearService {
+    AcademicYear getAcademicYearEntity(Long academicYearId);
 }
--- rc-application/src/main/java/com/transkript/reportcard/business/service/impl/AcademicYearServiceImpl.java
+++ rc-application/src/main/java/com/transkript/reportcard/business/service/impl/AcademicYearServiceImpl.java
@@ -1,12 +1,22 @@
 package com.transkript.reportcard.business.service.impl;
+
 import com.transkript.reportcard.business.service.AcademicYearService;
+import com.transkript.reportcard.data.entity.AcademicYear;
+import com.transkript.reportcard.data.repository.AcademicYearRepository;
+import com.transkript.reportcard.exception.EntityException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.springframework.stereotype.Service;
 
-
-@Getter
-@Setter
-@AllArgsConstructor
+@Service
+@RequiredArgsConstructor
 public class AcademicYearServiceImpl implements AcademicYearService {
+    private final AcademicYearRepository academicYearRepository;
+    @Override
+    public AcademicYear getAcademicYearEntity(Long academicYearId) {
+        return academicYearRepository.findById(academicYearId)
+                .orElseThrow(() -> new EntityException.EntityNotFoundException("academic year", academicYearId));
+    }
 }
