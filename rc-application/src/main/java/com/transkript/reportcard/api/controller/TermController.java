b5bc20355e153f359e7abbf3955e62ea49701a8e
Tanju Brunostar <tanjubrunostar0@gmail.com>
2022-05-18 17:43:56 +0100

    wrote a REST API for term

--- rc-application/src/main/java/com/transkript/reportcard/api/controller/TermController.java
+++ rc-application/src/main/java/com/transkript/reportcard/api/controller/TermController.java
@@ -1,11 +1,59 @@
 package com.transkript.reportcard.api.controller;
 
+import com.transkript.reportcard.api.dto.TermDto;
+import com.transkript.reportcard.business.service.TermService;
+import com.transkript.reportcard.data.entity.Term;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Slf4j
 @RestController
 @AllArgsConstructor
 @RequestMapping(value = "/api/term")
 public class TermController {
+
+    private final TermService termService;
+
+    @PostMapping
+    public ResponseEntity<String> addTerm(@RequestBody TermDto termDto) {
+        log.info("Adding a term with Name: " + termDto.getName());
+        return new ResponseEntity<String>(termService.addTerm(termDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TermDto>> getTerms() {
+        log.info("Getting all terms");
+        return ResponseEntity.ok(termService.getTerms());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TermDto> getTerm(
+            @PathVariable("id") Long id
+    ) {
+        log.info("Getting term with id: " + id);
+        return ResponseEntity.ok(termService.getTerm(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateTerm(
+            @PathVariable("id") Long id,
+            @RequestBody TermDto termDto
+    ) {
+        log.info("Updating term with id: " + id);
+        return ResponseEntity.ok(termService.updateTerm(id, termDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteTerm(
+            @PathVariable("id") Long id
+    ) {
+        log.info("Deleting term with id: " + id);
+        return ResponseEntity.ok(termService.deleteTerm(id));
+    }
 }
