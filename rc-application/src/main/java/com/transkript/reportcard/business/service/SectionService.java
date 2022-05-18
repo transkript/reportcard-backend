328acbfb9d6e70f0f8e6a60399f9bacf88bf2251
andrew21-mch <nfonandrew73@gmail.com>
2022-05-18 16:03:55 +0100

    modified the section service to get section by id

--- rc-application/src/main/java/com/transkript/reportcard/business/service/SectionService.java
+++ rc-application/src/main/java/com/transkript/reportcard/business/service/SectionService.java
@@ -2,11 +2,13 @@ package com.transkript.reportcard.business.service;
 
 
 import com.transkript.reportcard.api.dto.SectionDto;
+import com.transkript.reportcard.data.entity.Section;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface SectionService {
+    Section getSectionEntity(Long id);
     String addSection(SectionDto sectionDto);
 
     List<SectionDto> getSections();
