25316864ea0cd41509bee77748a352e4e6ea1cd5
Elroy Kanye <elroykdev@gmail.com>
2022-05-23 19:24:00 +0100

    send calculate average to report card

--- rc-process/src/main/kotlin/com/trankscript/reportcard/model/ReportCard.kt
+++ rc-process/src/main/kotlin/com/trankscript/reportcard/model/ReportCard.kt
@@ -1,5 +1,19 @@
 package com.trankscript.reportcard.model
 
 class ReportCard(val student: Student) {
-    var average: Double = 0.0;
+    var average: Double = 0.0
+
+    init {
+        calculateAverage()
+    }
+
+    private fun calculateAverage() {
+        var sumOfCoeff = 0
+        var coeffByGradeValue = 0.0
+        student.subjects.stream().peek{ subject: Subject ->
+            val subjectValue = (subject.grade.score * subject.coeff).toDouble()
+            coeffByGradeValue += subjectValue
+        }.map(Subject::coeff).forEach { coeff: Int -> sumOfCoeff += coeff }
+        average = coeffByGradeValue / sumOfCoeff
+    }
 }
