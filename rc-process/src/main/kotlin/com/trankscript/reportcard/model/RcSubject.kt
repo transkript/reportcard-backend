6d758dc3fa7f5bd3c690817e0ec001c93e857b81
Elroy Kanye <elroykdev@gmail.com>
2022-05-24 21:15:36 +0100

    add properties for generating report card

--- rc-process/src/main/kotlin/com/trankscript/reportcard/model/RcSubject.kt
+++ rc-process/src/main/kotlin/com/trankscript/reportcard/model/RcSubject.kt
@@ -1,7 +1,22 @@
 package com.trankscript.reportcard.model
 
-class RcSubject(val name: String, val coeff: Int, private val code: String, val rcGrade: RcGrade) {
+class RcSubject(val name: String, val coeff: Int, private val code: String, val rcGrades: List<RcGrade>) {
+    var score = 0.0;
+
+    init {
+        calcScore()
+    }
+
     override fun toString(): String {
-        return "RcSubject(name='$name', coeff=$coeff, code='$code', rcGrade=$rcGrade)"
+        return "RcSubject(name='$name', coeff=$coeff, code='$code', rcGrades=list)"
+    }
+
+    public fun calcScore() {
+        var gradeSum = 0.0
+        val gradeValues = rcGrades.map { rcGrade -> rcGrade.score * coeff }.toList()
+        for (value: Float in gradeValues) {
+            gradeSum += value
+        }
+        score = gradeSum / gradeValues.size
     }
 }
