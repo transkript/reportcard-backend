49a7c39f0f0ad6139b8b86b751fea859a9fdb22d
Elroy Kanye <elroykdev@gmail.com>
2022-05-24 01:06:35 +0100

    relate student application to application key and class level sub only

--- rc-application/src/main/java/com/transkript/reportcard/data/entity/StudentApplication.java
+++ rc-application/src/main/java/com/transkript/reportcard/data/entity/StudentApplication.java
@@ -1,24 +1,22 @@
 package com.transkript.reportcard.data.entity;
 
 
-import com.transkript.reportcard.data.entity.relation.SubjectRegistration;
+import com.transkript.reportcard.data.entity.composite.ApplicationKey;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -27,9 +25,10 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
+@Builder
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Table(name = "student_application")
 public class StudentApplication {
     @EmbeddedId
@@ -41,27 +40,30 @@ public class StudentApplication {
 
     @Builder.Default
     @UpdateTimestamp
-    @Column(name = "updated_at", nullable = false, updatable = false)
+    @Column(name = "updated_at", nullable = false, updatable = true)
     private LocalDateTime updatedAt = LocalDateTime.now();
 
+    @MapsId("studentId")
     @ManyToOne(optional = false)
     @JoinColumn(name = "student_id", nullable = false)
     private Student student;
 
+    @MapsId("yearId")
     @ManyToOne(optional = false)
     @JoinColumn(name = "academic_year_id", nullable = false)
     private AcademicYear academicYear;
 
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "class_level_sub_id", nullable = false)
+    private ClassLevelSub classLevelSub;
+
+    @OneToMany(mappedBy = "studentApplication", orphanRemoval = true)
+    @ToString.Exclude
+    private List<SubjectRegistration> subjectRegistrations = new ArrayList<>();
+    /*
     @Builder.Default
     @OneToMany(mappedBy = "studentApplication", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<SubjectRegistration> subjectRegistrations = new ArrayList<>();
 
-    @OneToOne(optional = false, orphanRemoval = true)
-    @JoinColumn(name = "class_level_id", nullable = false)
-    private ClassLevel classLevel;
-
-    @OneToOne(optional = false, orphanRemoval = true)
-    @JoinColumn(name = "class_level_sub_id", nullable = false)
-    private ClassLevelSub classLevelSub;
-
+     */
 }
