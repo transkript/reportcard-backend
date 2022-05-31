fab5d4d8270bc5c7bb46ebb743ea9d2c309f881a
Elroy Kanye <elroykdev@gmail.com>
2022-05-31 17:36:44 +0100

    update api struct for rc controller

--- rc-application/src/main/java/com/transkript/reportcard/api/controller/RcController.java
+++ rc-application/src/main/java/com/transkript/reportcard/api/controller/RcController.java
@@ -1,14 +1,16 @@
 package com.transkript.reportcard.api.controller;
 
+import com.transkript.reportcard.api.dto.request.ReportCardRequest;
 import com.transkript.reportcard.business.service.RcService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 @Slf4j
@@ -19,7 +21,7 @@ public class RcController {
     private final RcService rcService;
 
     @GetMapping(value = "/reportcard/{termId}")
-    public void getReportCard(@NotNull @RequestParam Long studentId, @NotNull @RequestParam Long yearId, @NotNull @PathVariable Long termId) {
-        rcService.getReportCard(studentId, yearId, termId);
+    public void getReportCard(@NotNull @PathVariable Long termId, @Valid @RequestBody ReportCardRequest reportCardRequest) {
+        rcService.getReportCard(termId, reportCardRequest);
     }
 }
