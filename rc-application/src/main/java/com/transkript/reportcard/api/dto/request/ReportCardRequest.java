a08f6660d1c0c0737deeb2c2a2459ea9f4dc9d3f
Elroy Kanye <elroykdev@gmail.com>
2022-06-01 17:41:57 +0100

    add request object for rc

---
+++ rc-application/src/main/java/com/transkript/reportcard/api/dto/request/ReportCardRequest.java
@@ -0,0 +1,19 @@
+package com.transkript.reportcard.api.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+public record ReportCardRequest(
+        @NotNull @JsonProperty(value = "student_id") Long studentId,
+        @NotNull @JsonProperty(value = "year_id") Long yearId,
+        @NotNull @JsonProperty(value = "sequences") SequenceRequest sequenceRequest
+) implements Serializable {
+    public record SequenceRequest(
+            @JsonProperty(value = "x_sequence") Long openingSequenceId,
+            @JsonProperty(value = "y_sequence") Long closingSequenceId
+    ) implements Serializable {}
+}
+
+
