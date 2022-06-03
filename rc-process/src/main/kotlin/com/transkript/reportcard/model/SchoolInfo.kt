932d77542c58cdd7afcae767ac7335e4715accb1
Elroy Kanye <elroykdev@gmail.com>
2022-06-03 16:40:09 +0100

    add sequence names to school

--- rc-process/src/main/kotlin/com/transkript/reportcard/model/SchoolInfo.kt
+++ rc-process/src/main/kotlin/com/transkript/reportcard/model/SchoolInfo.kt
@@ -1,6 +1,6 @@
 package com.transkript.reportcard.model
 
-class SchoolInfo(val name: String, val section: String, val yearName: String, val termName: String) {
+class SchoolInfo(val name: String, val section: String, val yearName: String, val termName: String, val seqAName: String, val seqBName: String) {
     override fun toString(): String {
         return "RcSchool(name='$name', section='$section')"
     }
