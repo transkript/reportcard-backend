0e76f4c58932b5734d3f41ae0bf0b6cd60dd1564
Elroy Kanye <elroykdev@gmail.com>
2022-06-11 13:33:54 +0100

    add method and impl for school settings service

---
+++ rc-application/src/main/java/com/transkript/reportcard/business/service/impl/SchoolSettingsServiceImpl.java
@@ -0,0 +1,68 @@
+package com.transkript.reportcard.business.service.impl;
+
+import com.transkript.reportcard.api.dto.SchoolSettingsDto;
+import com.transkript.reportcard.api.dto.response.EntityResponse;
+import com.transkript.reportcard.business.mapper.SchoolSettingsMapper;
+import com.transkript.reportcard.business.service.AcademicYearService;
+import com.transkript.reportcard.business.service.SchoolSettingsService;
+import com.transkript.reportcard.business.service.SequenceService;
+import com.transkript.reportcard.business.service.TermService;
+import com.transkript.reportcard.data.entity.AcademicYear;
+import com.transkript.reportcard.data.entity.SchoolSettings;
+import com.transkript.reportcard.data.entity.Sequence;
+import com.transkript.reportcard.data.entity.Term;
+import com.transkript.reportcard.data.repository.SchoolSettingsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class SchoolSettingsServiceImpl implements SchoolSettingsService {
+    private final SchoolSettingsRepository schoolSettingsRepository;
+    private final SchoolSettingsMapper schoolSettingsMapper;
+    private final SequenceService sequenceService;
+    private final TermService termService;
+    private final AcademicYearService academicYearService;
+
+    @Override
+    @Transactional
+    public EntityResponse addSettings(SchoolSettingsDto schoolSettingsDto) {
+        SchoolSettings schoolSettings = schoolSettingsMapper.mapDtoToSchoolSettings(schoolSettingsDto);
+
+        {
+            Sequence currSequence = sequenceService.getSequenceEntity(schoolSettingsDto.currentSequenceId());
+            Term currTerm = termService.getTermEntity(schoolSettingsDto.currentTermId());
+            AcademicYear currAcademicYear = academicYearService.getAcademicYearEntity(schoolSettingsDto.currentYearId());
+
+            schoolSettings.setCurrentSequence(currSequence);
+            schoolSettings.setCurrentTerm(currTerm);
+            schoolSettings.setCurrentAcademicYear(currAcademicYear);
+        }
+
+        if(schoolSettingsRepository.findAll().size() == 0) {
+            schoolSettings.setApplicationOpen(schoolSettings.getApplicationOpen() != null && schoolSettings.getApplicationOpen());
+            schoolSettingsRepository.save(schoolSettings);
+        } else {
+            schoolSettingsRepository.findAll().stream().findFirst().ifPresentOrElse(
+                    settings -> {
+                        if(!Objects.equals(settings.getMaxGradeScore(), schoolSettingsDto.maxGrade())) {
+                            settings.setMaxGradeScore(schoolSettingsDto.maxGrade());
+                        }
+                        if(!Objects.equals(settings.getMinGradeScore(), schoolSettingsDto.minGrade())) {
+                            settings.setMinGradeScore(schoolSettingsDto.minGrade());
+                        }
+
+                    },
+                    () -> schoolSettingsRepository.save(schoolSettings)
+            );
+        }
+
+
+        return EntityResponse.builder().id(
+                schoolSettingsRepository.findAll().stream().findFirst().orElse(schoolSettings).getId()
+        ).message("Successfully added settings for the system").entityName("school settings").build();
+    }
+}
