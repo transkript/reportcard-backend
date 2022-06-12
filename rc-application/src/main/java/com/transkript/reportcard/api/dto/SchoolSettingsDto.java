f5cc03a909445bc3efb6b63774127bd495430f53
Elroy Kanye <elroykdev@gmail.com>
2022-06-12 14:16:40 +0100

    restrict school settings props from being null

--- rc-application/src/main/java/com/transkript/reportcard/api/dto/SchoolSettingsDto.java
+++ rc-application/src/main/java/com/transkript/reportcard/api/dto/SchoolSettingsDto.java
@@ -4,15 +4,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 public record SchoolSettingsDto (
-        @JsonProperty("id") Long id,
-        @JsonProperty("curr_year_id") Long currentYearId,
-        @JsonProperty("curr_term_id") Long currentTermId,
-        @JsonProperty("curr_seq_id") Long currentSequenceId,
-        @Min(value = 0) @JsonProperty("min_grade") Long minGrade,
-        @Max(value = 100)@JsonProperty("max_grade") Long maxGrade,
-        @JsonProperty("application_is_open") Boolean applicationOpen
+        @NotNull @JsonProperty("id") Long id,
+        @NotNull @JsonProperty("curr_year_id") Long currentYearId,
+        @NotNull @JsonProperty("curr_term_id") Long currentTermId,
+        @NotNull @JsonProperty("curr_seq_id") Long currentSequenceId,
+        @NotNull @Min(value = 0) @JsonProperty("min_grade") Long minGrade,
+        @NotNull @Max(value = 100)@JsonProperty("max_grade") Long maxGrade,
+        @NotNull @JsonProperty("application_is_open") Boolean applicationOpen
 )
 implements Serializable {}
