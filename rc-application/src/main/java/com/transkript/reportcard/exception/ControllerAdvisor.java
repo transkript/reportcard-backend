bc74ba2c77629da8cda1084532c0f2e367da75c2
Elroy Kanye <elroykdev@gmail.com>
2022-06-12 14:29:56 +0100

    add handler for sql data integrity error

--- rc-application/src/main/java/com/transkript/reportcard/exception/ControllerAdvisor.java
+++ rc-application/src/main/java/com/transkript/reportcard/exception/ControllerAdvisor.java
@@ -1,8 +1,10 @@
-package com.transkript.reportcard.exception.advice;
+package com.transkript.reportcard.exception;
 
 import com.transkript.reportcard.exception.EntityException;
 import com.transkript.reportcard.exception.ReportCardException;
 import com.transkript.reportcard.exception.body.ExceptionBody;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,14 +28,20 @@ public class ControllerAdvisor extends ResponseEntityExceptionHandler {
      * @param request
      * @return
      */
+    @NotNull
     @Override
-    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
+            @NotNull HttpRequestMethodNotSupportedException ex,
+            @NotNull HttpHeaders headers, @NotNull HttpStatus status, @NotNull WebRequest request) {
         return super.handleHttpRequestMethodNotSupported(ex, headers, status, request);
     }
 
     // Override methods
+    @NotNull
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, @NotNull HttpHeaders headers,
+            @NotNull HttpStatus status, @NotNull WebRequest request) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
@@ -46,6 +54,13 @@ public class ControllerAdvisor extends ResponseEntityExceptionHandler {
                         request, HttpStatus.BAD_REQUEST, errors),
                 HttpStatus.BAD_REQUEST
         );
+    }
+
+    @ExceptionHandler(value = {DataIntegrityViolationException.class})
+    protected ResponseEntity<ExceptionBody> handleDataIntegrityViolationException(
+            DataIntegrityViolationException ex, WebRequest webRequest
+    ) {
+        return getExceptionEntity(ex, webRequest, HttpStatus.INTERNAL_SERVER_ERROR, Map.of());
     }
 
     // ReportCard Exception handlers
