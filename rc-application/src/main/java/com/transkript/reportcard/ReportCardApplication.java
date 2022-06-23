29c4f27677aafd9fca12be995a3eb9a52cd6c679
Elroy Kanye <elroykdev@gmail.com>
2022-06-23 05:42:19 +0100

    add method for preparing working dir of application

--- rc-application/src/main/java/com/transkript/reportcard/ReportCardApplication.java
+++ rc-application/src/main/java/com/transkript/reportcard/ReportCardApplication.java
@@ -1,13 +1,34 @@
 package com.transkript.reportcard;
 
+import com.transkript.reportcard.config.constants.AppConstants;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Slf4j
 @SpringBootApplication
 public class ReportCardApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ReportCardApplication.class, args);
+        prepareWorkingDir();
     }
 
+    private static void prepareWorkingDir() {
+        File settingsFile = new File(AppConstants.settingsFile);
+        if(!settingsFile.exists()) {
+            try {
+                Files.createFile(Path.of(settingsFile.getAbsolutePath()));
+                log.info("Settings file created");
+            } catch (IOException e) {
+                log.error(e.getMessage());
+            }
+        } else {
+            log.info("Settings file already exists");
+        }
+    }
 }
