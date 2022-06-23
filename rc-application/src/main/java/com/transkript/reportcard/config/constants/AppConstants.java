e0a2a11b850d93e422c3bcf14bb8a935ae39f3c2
Elroy Kanye <elroykdev@gmail.com>
2022-06-23 05:38:32 +0100

    create constants package

---
+++ rc-application/src/main/java/com/transkript/reportcard/config/constants/AppConstants.java
@@ -0,0 +1,6 @@
+package com.transkript.reportcard.config.constants;
+
+public class AppConstants {
+    public static final String workingDir = "rcapp";
+    public static final String settingsFile = workingDir + "/settings.json";
+}
--- rc-application/src/main/java/com/transkript/reportcard/config/constants/EntityName.java
+++ rc-application/src/main/java/com/transkript/reportcard/config/constants/EntityName.java
@@ -3,4 +3,5 @@ package com.transkript.reportcard.config.constants;
 public class EntityName {
     public static final String CLASS_LEVEL = "class level";
     public static final String CLASS_LEVEL_SUB = "class level sub";
+    public static final String STUDENT = "student";
 }
