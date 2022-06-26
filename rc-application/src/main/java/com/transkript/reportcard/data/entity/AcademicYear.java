4e15986fadae971c459257fad135f9e97db0fefa
Elroy Kanye <elroykdev@gmail.com>
2022-06-26 06:40:14 +0100

    remove link to student application

--- rc-application/src/main/java/com/transkript/reportcard/data/entity/AcademicYear.java
+++ rc-application/src/main/java/com/transkript/reportcard/data/entity/AcademicYear.java
@@ -9,19 +9,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
 
 @Entity
 @Getter
@@ -38,11 +32,6 @@ public class AcademicYear {
 
     @Column(nullable = false, name = "year_name", unique = true)
     private String name;
-
-    @OneToMany(mappedBy = "academicYear", cascade = CascadeType.ALL, orphanRemoval = true)
-    @Builder.Default
-    @ToString.Exclude
-    private List<StudentApplication> studentApplications = new ArrayList<>();
 
     @Embedded
     private AcademicInfo academicInfo;
