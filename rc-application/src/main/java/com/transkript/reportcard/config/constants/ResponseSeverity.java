41642ec1cd3610c94ef43a785bc1940dfc414a5d
Elroy Kanye <elroykdev@gmail.com>
2022-06-27 06:46:41 +0100

    add a response severity class

---
+++ rc-application/src/main/java/com/transkript/reportcard/config/constants/ResponseSeverity.java
@@ -0,0 +1,7 @@
+package com.transkript.reportcard.config.constants;
+
+public class ResponseSeverity {
+    public final static String SUCCESS = "success";
+    public final static String WARN = "warn";
+    public final static String ERROR = "error";
+}
