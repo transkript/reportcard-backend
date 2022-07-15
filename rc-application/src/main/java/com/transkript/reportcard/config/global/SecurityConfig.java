f183d159b1c90da31a2a58f0a897aabbe02ca99d
Elroy Kanye <elroykanye@gmail.com>
2022-07-15 10:05:00 +0100

    remove JwtSecurityFilter bean

--- rc-application/src/main/java/com/transkript/reportcard/config/global/SecurityConfig.java
+++ rc-application/src/main/java/com/transkript/reportcard/config/global/SecurityConfig.java
@@ -1,8 +1,8 @@
 package com.transkript.reportcard.config.global;
 
-import com.transkript.reportcard.api.filter.JwtSecurityFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,12 +15,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
 import org.springframework.session.web.http.HttpSessionIdResolver;
 
+@Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
-    private final JwtSecurityFilter jwtSecurityFilter;
     private final UserAuthEntryPoint userAuthEntryPoint;
 
     @Override @Bean
