981e14c46f7b2836b01a66e0608d1a1e6caa8baf
Elroy Kanye <elroykanye@gmail.com>
2022-07-15 10:05:49 +0100

    add failure logging

--- rc-application/src/main/java/com/transkript/reportcard/config/global/UserAuthEntryPoint.java
+++ rc-application/src/main/java/com/transkript/reportcard/config/global/UserAuthEntryPoint.java
@@ -1,5 +1,6 @@
 package com.transkript.reportcard.config.global;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -9,10 +10,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Slf4j
 @Component
 public class UserAuthEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authenticationException) throws IOException, ServletException {
+        log.error("Unauthorized error. Message - {}", authenticationException.getMessage());
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized user");
     }
 }
