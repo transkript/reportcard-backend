dc198b7eb9321368d72ddfb5542f7d10ac260373
Elroy Kanye <elroykanye@gmail.com>
2022-07-21 10:12:51 +0100

    adjust import from change of package name interf -> i

--- rc-application/src/main/java/com/transkript/reportcard/api/controller/SchoolController.java
+++ rc-application/src/main/java/com/transkript/reportcard/api/controller/SchoolController.java
@@ -2,7 +2,7 @@ package com.transkript.reportcard.api.controller;
 
 import com.transkript.reportcard.api.dto.SchoolDto;
 import com.transkript.reportcard.api.dto.response.EntityResponse;
-import com.transkript.reportcard.business.service.interf.SchoolService;
+import com.transkript.reportcard.business.service.i.SchoolService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
