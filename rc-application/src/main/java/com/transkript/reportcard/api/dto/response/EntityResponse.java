dc064a3a71a9671e6479bd45f3a72fd6009d0454
Elroy Kanye <elroykanye@gmail.com>
2022-07-31 00:20:01 +0100

    optimise EntityResponse

--- rc-application/src/main/java/com/transkript/reportcard/api/dto/response/EntityResponse.java
+++ rc-application/src/main/java/com/transkript/reportcard/api/dto/response/EntityResponse.java
@@ -6,14 +6,9 @@ import lombok.Builder;
 import java.io.Serializable;
 import java.util.Map;
 
-@Builder
 public record EntityResponse(
-        @JsonProperty("id") Long id,
-        @JsonProperty(value = "ids") Map<String, Object> ids,
+        @JsonProperty("id") Object id,
         @JsonProperty(value = "message") String message,
-        @JsonProperty(value = "entity_name") String entityName,
-        @JsonProperty(value = "status") Integer status,
-        @JsonProperty(value = "log") Boolean log,
-        @JsonProperty(value = "severity") String severity
+        @JsonProperty(value = "log") Boolean log
 ) implements Serializable {
 }
